62760cd9aa1201d42727cd8d6f1ab21ab4f00f86
k1mjunyoung <vovrwm@gmail.com>
2023-12-04 10:41:34 +0900

    Initial commit

---
+++ src/main/java/slowxmas/SlowXmasApplication.java
@@ -0,0 +1,13 @@
+package slowxmas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlowXmasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlowXmasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/slowxmas/SlowXmasApplicationTests.java
@@ -0,0 +1,13 @@
+package slowxmas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlowXmasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
